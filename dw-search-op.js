cad1689bb8eec227e40bad78b6b1380fb3b7e8ee
Leif Ershag <leif@ershag.se>
2024-05-17 20:34:14 +0000

    Added script to search drachenwald op.

---
+++ dw-search-op.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name Search Drachenwald OP
+// @namespace http://tampermonkey.net/
+// @version 1.0
+// @description Adds a context menu item to search for a persona in the OP.
+// @author Leif Ershag
+// @match *://*/*
+// @grant GM_registerMenuCommand
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function submitSearchForm(query) {
+        const form = document.createElement('form');
+        form.method = 'POST';
+        form.action = 'https://op.drachenwald.sca.org/search';
+        form.target = '_blank'; // Open form submission in a new tab
+
+        const input = document.createElement('input');
+        input.type = 'hidden';
+        input.name = 'persona';
+        input.value = query;
+
+        form.appendChild(input);
+        document.body.appendChild(form);
+        form.submit();
+        document.body.removeChild(form);
+    }
+
+    function searchOp() {
+        var name = window.getSelection().toString();
+        submitSearchForm(name);
+    }
+
+    GM_registerMenuCommand('Search OP', function(x,y) {
+        searchOp();
+    });
+})();
